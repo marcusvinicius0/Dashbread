16ca081c797a922f6483a459ca3c9c56020dce86
marcusvinicius0 <begheli2020@gmail.com>
2024-08-17 15:13:56 -0300

    feat: accessibility with dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className="dark antialiased">
       <head>
         <link rel="icon" href="/favicon.svg" />
       </head>
       <body className={inter.className}>
-        <div className="min-h-screen lg:grid lg:grid-cols-app">
+        <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
           <Sidebar />
           <main className="max-w-screen px-4 pb-12 pt-24 lg:col-start-2 lg:w-auto lg:px-8 lg:pt-8">
             {children}
